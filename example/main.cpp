e49104d5ed89e06e29b4c24b1dd8202bee25b0ce
GoshaSerbin <gosha_serbin00@mail.ru>
2025-05-24 18:13:41 +0300

    style: applied clang formatting

--- example/main.cpp
+++ example/main.cpp
@@ -8,19 +8,19 @@ using namespace ttie;
 
 int main()
 {
-//     Tensor a(std::vector<float>({4, 3, 2, 1}), true);
-//     Tensor b(std::vector<float>({1, 2, 3, 4}), true);
-//     a.reshape({1, 2, 2});
-//     b.reshape({1, 2, 2});
-//
-//     Tensor c = Tensor::log(a);
-//
-//     std::cout << "c:" << std::endl;
-//     c.backward();
-//     std::cout << "backwarded" << std::endl;
-//     std::cout << a[{0, 1, 0}] << std::endl;
-//     std::cout << a << std::endl;
-//     std::cout << c << std::endl;
+    //     Tensor a(std::vector<float>({4, 3, 2, 1}), true);
+    //     Tensor b(std::vector<float>({1, 2, 3, 4}), true);
+    //     a.reshape({1, 2, 2});
+    //     b.reshape({1, 2, 2});
+    //
+    //     Tensor c = Tensor::log(a);
+    //
+    //     std::cout << "c:" << std::endl;
+    //     c.backward();
+    //     std::cout << "backwarded" << std::endl;
+    //     std::cout << a[{0, 1, 0}] << std::endl;
+    //     std::cout << a << std::endl;
+    //     std::cout << c << std::endl;
 
     Tensor t;
     std::cout << t.empty();
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,14 +1,16 @@
-#include <gtest/gtest.h>
 #include "ttie/ttie.h"
+#include <gtest/gtest.h>
 
 using namespace ttie;
 
-TEST(TensorTest, UninitializedTensor) {
+TEST(TensorTest, UninitializedTensor)
+{
     Tensor x;
     EXPECT_TRUE(x.empty());
 }
 
-TEST(TensorTest, BasicOps) {
+TEST(TensorTest, BasicOps)
+{
     Tensor a(std::vector<float>({1, 2, 3, 4}));
     Tensor b(std::vector<float>({1, 2, 3, 4}));
     a.reshape({1, 2, 2});
@@ -17,14 +19,16 @@ TEST(TensorTest, BasicOps) {
     EXPECT_THROW(a + b, std::exception);
 }
 
-TEST(TensorTest, InitializeAndSetData) {
+TEST(TensorTest, InitializeAndSetData)
+{
     Tensor t;
     t.reshape({2, 3});
 
     EXPECT_EQ(t.size(), 6);
 }
 
-TEST(AutoGradTest, AddOp_) {
+TEST(AutoGradTest, AddOp_)
+{
     Tensor a({1.0f, 1.0f, 1.0f, 1.0f}, true);
     Tensor b({1.0f, 1.0f, 1.0f, 1.0f}, true);
 
@@ -45,7 +49,8 @@ TEST(AutoGradTest, AddOp_) {
     EXPECT_EQ(b_grad[3], 1);
 }
 
-TEST(AutoGradTest, MulOp_) {
+TEST(AutoGradTest, MulOp_)
+{
     Tensor a({1.0f, 2.0f, 3.0f, 4.0f}, true);
     Tensor b({5.0f, 6.0f, 7.0f, 8.0f}, true);
 
@@ -66,7 +71,8 @@ TEST(AutoGradTest, MulOp_) {
     EXPECT_NEAR(b_grad[3], 4.0, 1e-6);
 }
 
-TEST(AutoGradTest, SubOp_) {
+TEST(AutoGradTest, SubOp_)
+{
     Tensor a({1.0f, 1.0f, 1.0f, 1.0f}, true);
     Tensor b({1.0f, 1.0f, 1.0f, 1.0f}, true);
 
@@ -87,7 +93,8 @@ TEST(AutoGradTest, SubOp_) {
     EXPECT_NEAR(b_grad[3], -1.0, 1e-6);
 }
 
-TEST(AutoGradTest, UnaryMinusOp_) {
+TEST(AutoGradTest, UnaryMinusOp_)
+{
     Tensor a({2.0f, 1.0f, 2.0f, 1.0f}, true);
 
     Tensor c = -a;
@@ -102,7 +109,8 @@ TEST(AutoGradTest, UnaryMinusOp_) {
     EXPECT_NEAR(a_grad[3], -1.0, 1e-6);
 }
 
-TEST(AutoGradTest, MaxOp_) {
+TEST(AutoGradTest, MaxOp_)
+{
     Tensor a({2.0f, 1.0f, 2.0f, 1.0f}, true);
     Tensor b({1.0f, 2.0f, 1.0f, 2.0f}, true);
 
@@ -123,7 +131,8 @@ TEST(AutoGradTest, MaxOp_) {
     EXPECT_NEAR(b_grad[3], 1.0, 1e-6);
 }
 
-TEST(AutoGradTest, SqrtOp_) {
+TEST(AutoGradTest, SqrtOp_)
+{
     Tensor a({2.0f, 1.0f, 2.0f, 1.0f}, true);
 
     Tensor c = Tensor::sqrt(a);
@@ -138,7 +147,8 @@ TEST(AutoGradTest, SqrtOp_) {
     EXPECT_NEAR(a_grad[3], 0.5, 1e-5);
 }
 
-TEST(AutoGradTest, LogOp_) {
+TEST(AutoGradTest, LogOp_)
+{
     Tensor a({2.0f, 1.0f, 2.0f, 1.0f}, true);
 
     Tensor c = Tensor::log(a);
@@ -153,7 +163,8 @@ TEST(AutoGradTest, LogOp_) {
     EXPECT_NEAR(a_grad[3], 1.0, 1e-5);
 }
 
-TEST(AutoGradTest, DivOp_) {
+TEST(AutoGradTest, DivOp_)
+{
     Tensor a({1.0f, 1.0f, 1.0f, 1.0f}, true);
     Tensor b({2.0f, 2.0f, 2.0f, 2.0f}, true);
 
@@ -174,7 +185,8 @@ TEST(AutoGradTest, DivOp_) {
     EXPECT_NEAR(b_grad[3], -0.25, 1e-6);
 }
 
-TEST(AutoGradTest, MatmulOp) {
+TEST(AutoGradTest, MatmulOp)
+{
     Tensor a({1.5f, 1.0f, 1.5f}, true);
     Tensor b({1.0f, 1.5f, 1.0f}, true);
 
@@ -196,7 +208,8 @@ TEST(AutoGradTest, MatmulOp) {
     EXPECT_NEAR(b_grad[2], 1.5f, 1e-6);
 }
 
-TEST(AutoGradTest, BroadcastOp_) {
+TEST(AutoGradTest, BroadcastOp_)
+{
     Tensor a({1.5f, 1.0f, 1.5f}, true);
 
     a.reshape({3});
@@ -212,20 +225,23 @@ TEST(AutoGradTest, BroadcastOp_) {
     EXPECT_NEAR(a_grad[2], 2.0f, 1e-6);
 }
 
-TEST(LayerTest, ReLU) {
+TEST(LayerTest, ReLU)
+{
     ReLU relu;
     EXPECT_EQ(relu.parameters().size(), 0);
 
     Tensor input;
     input.reshape({2, 3});
-    for (size_t i = 0; i < input.size(); ++i) {
+    for (size_t i = 0; i < input.size(); ++i)
+    {
         input[i] = static_cast<float>(i) - 2.0f;
     }
 
     Tensor output;
     relu.forward(input, output);
 
-    for (size_t i = 0; i < output.size(); ++i) {
+    for (size_t i = 0; i < output.size(); ++i)
+    {
         EXPECT_FLOAT_EQ(output[i], std::max(0.0f, input[i]));
     }
 
@@ -234,12 +250,14 @@ TEST(LayerTest, ReLU) {
     auto grad_ = output.get_grad();
 
     auto input_grad = input.get_grad();
-    for(size_t i = 0; i < input.size(); ++i) {
+    for (size_t i = 0; i < input.size(); ++i)
+    {
         EXPECT_FLOAT_EQ(input_grad[i], input[i] >= 0 ? 1.0f : 0.0f);
     }
 }
 
-TEST(LayerTest, Sigmoid) {
+TEST(LayerTest, Sigmoid)
+{
     Sigmoid sigmoid;
     Tensor input({0.0f, 1.0f}, true);
 
@@ -253,11 +271,12 @@ TEST(LayerTest, Sigmoid) {
 
     auto grad = input.get_grad();
 
-    EXPECT_NEAR(grad[0], 0.25f, 1e-5f);        // σ'(0) * 1 = 0.25
-    EXPECT_NEAR(grad[1], 0.19661193f, 1e-5f);  // σ'(1) * 1 ≈ 0.197
+    EXPECT_NEAR(grad[0], 0.25f, 1e-5f);       // σ'(0) * 1 = 0.25
+    EXPECT_NEAR(grad[1], 0.19661193f, 1e-5f); // σ'(1) * 1 ≈ 0.197
 }
 
-TEST(LayerTest, Tanh) {
+TEST(LayerTest, Tanh)
+{
     Tanh tanh;
     Tensor input({0.0f, 1.0f}, true);
     input.reshape({2});
@@ -276,8 +295,8 @@ TEST(LayerTest, Tanh) {
     EXPECT_NEAR(grad[1], 0.41997434f, 1e-5f); // tanh'(1) ≈ 0.420
 }
 
-
-TEST(LayerTest, Linear) {
+TEST(LayerTest, Linear)
+{
     Linear linear(3, 2);
 
     auto params = linear.parameters();
@@ -291,7 +310,8 @@ TEST(LayerTest, Linear) {
 
     Tensor input(true);
     input.reshape({2, 3});
-    for (size_t i = 0; i < input.size(); ++i) {
+    for (size_t i = 0; i < input.size(); ++i)
+    {
         input[i] = static_cast<float>(i) / input.size();
     }
 
@@ -302,9 +322,11 @@ TEST(LayerTest, Linear) {
 
     EXPECT_EQ(shape_.size(), 2);
     EXPECT_EQ(shape_[0], 2);
-    EXPECT_EQ(shape_[1], 2); // w.shape = [3, 2], input.shape = [2, 3] => (input @ weight).shape = [2, 2]
+    EXPECT_EQ(shape_[1], 2); // w.shape = [3, 2], input.shape = [2, 3] => (input
+                             // @ weight).shape = [2, 2]
 
-    for (size_t i = 0; i < output.size(); ++i) {
+    for (size_t i = 0; i < output.size(); ++i)
+    {
         output[i] = 1.0f;
     }
 
@@ -314,8 +336,10 @@ TEST(LayerTest, Linear) {
 
     bool has_nonzero = false;
     auto inp_grad = input.get_grad();
-    for (size_t i = 0; i < input.size(); ++i) {
-        if(std::abs(inp_grad[i]) > 1e-5) {
+    for (size_t i = 0; i < input.size(); ++i)
+    {
+        if (std::abs(inp_grad[i]) > 1e-5)
+        {
             has_nonzero = true;
             break;
         }
@@ -324,8 +348,10 @@ TEST(LayerTest, Linear) {
 
     has_nonzero = false;
     auto w_grad = linear.weight.get_grad();
-    for (size_t i = 0; i < linear.weight.size(); ++i) {
-        if (std::fabs(w_grad[i]) > 1e-8) {
+    for (size_t i = 0; i < linear.weight.size(); ++i)
+    {
+        if (std::fabs(w_grad[i]) > 1e-8)
+        {
             has_nonzero = true;
             break;
         }
@@ -334,8 +360,10 @@ TEST(LayerTest, Linear) {
 
     has_nonzero = false;
     auto bias_grad = linear.bias.get_grad();
-    for (size_t i = 0; i < bias_grad.size(); ++i) {
-        if (std::fabs(bias_grad[i]) > 1e-8) {
+    for (size_t i = 0; i < bias_grad.size(); ++i)
+    {
+        if (std::fabs(bias_grad[i]) > 1e-8)
+        {
             has_nonzero = true;
             break;
         }
@@ -343,13 +371,14 @@ TEST(LayerTest, Linear) {
     EXPECT_TRUE(has_nonzero);
 }
 
-TEST(LayerTest, LinearVSTorch) {
+TEST(LayerTest, LinearVSTorch)
+{
     /* PyTorch reference forward and backward with predefined weights and biases
     import torch
 
     input = torch.tensor([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]], requires_grad=True)
-    weight = torch.tensor([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]], requires_grad=True)
-    bias = torch.tensor([0.1, 0.2], requires_grad=True)
+    weight = torch.tensor([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]],
+    requires_grad=True) bias = torch.tensor([0.1, 0.2], requires_grad=True)
 
     output = torch.addmm(bias, input, weight)
     output.backward(torch.ones_like(output))
@@ -381,7 +410,6 @@ TEST(LayerTest, LinearVSTorch) {
 
     Tensor input({0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f});
     input.reshape({2, 3});
-
 
     // Forward pass
     Tensor output;
@@ -429,13 +457,14 @@ TEST(LayerTest, LinearVSTorch) {
     EXPECT_NEAR(bias_grad[1], 2.0f, 1e-5f);
 }
 
-TEST(ModelTest, ForwardAndBackwardVSTorch) {
+TEST(ModelTest, ForwardAndBackwardVSTorch)
+{
     /* Pytorch reference
     import torch
 
     input = torch.tensor([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]], requires_grad=True)
-    weight = torch.tensor([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]], requires_grad=True)
-    bias = torch.tensor([0.1, 0.2], requires_grad=True)
+    weight = torch.tensor([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]],
+    requires_grad=True) bias = torch.tensor([0.1, 0.2], requires_grad=True)
 
     # First addmm operation
     output1 = torch.addmm(bias, input, weight)
@@ -484,12 +513,12 @@ TEST(ModelTest, ForwardAndBackwardVSTorch) {
     model.add_layer(new ReLU());
     model.add_layer(new Linear(2, 1));
 
-    Linear* layer1 = static_cast<Linear*>(model.layers[0]);
+    Linear *layer1 = static_cast<Linear *>(model.layers[0]);
     layer1->weight.init({0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f});
     layer1->weight.reshape({3, 2});
     layer1->bias.init({0.1f, 0.2f});
 
-    Linear* layer2 = static_cast<Linear*>(model.layers[2]);
+    Linear *layer2 = static_cast<Linear *>(model.layers[2]);
     layer2->weight.init({0.7f, 0.8f});
     layer2->weight.reshape({2, 1});
     layer2->bias.init({0.3f});
@@ -548,7 +577,8 @@ TEST(ModelTest, ForwardAndBackwardVSTorch) {
     EXPECT_NEAR(l2_bias_grad[0], 2.0000f, 1e-4f);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
